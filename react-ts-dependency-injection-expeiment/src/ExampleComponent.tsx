7ea8e525ce374bd1d06ffacfb087c7464e65af06
Viktor Faddi <faddiv@gmail.com>
2020-12-21 14:19:47 +0100

    Create useService and useServices.

--- react-ts-dependency-injection-expeiment/src/ExampleComponent.tsx
+++ react-ts-dependency-injection-expeiment/src/ExampleComponent.tsx
@@ -1,6 +1,14 @@
 import { FunctionComponent } from "react";
 import { injector } from "./react-injector/injectServices";
 
+const { withServices, useService, useServices } = injector({
+  service1: () => "Hello",
+  service2: () => 42,
+  service3: () => new String("from service"),
+  service4: () => "from service",
+  service5: () => {return { important: "Do remember" };}
+});
+
 interface ExampleComponentProps {
   service1: any,
   service3: any,
@@ -8,18 +16,14 @@ interface ExampleComponentProps {
 }
 
 const ExampleComponent: FunctionComponent<ExampleComponentProps> = ({ service1, service3, otherProp }) => {
+  const s2 = useService("service2");
+  const service5 = useService("service5");
+  const { service2, service4 } = useServices("service2", "service4")
+
   return (
-    <div>{service1} {service3} {otherProp}</div>
+    <div>{service1} {service3} {otherProp} {s2} {service2} {service5.important} {service4}</div>
   );
 };
-
-const { withServices } = injector({
-  service1: () => "Hello",
-  service2: () => "Hello2",
-  service3: () => new String("from service"),
-  service4: () => "from service",
-  service5: () => "from service"
-});
 
 const NewComponent = withServices("service1", "service3")(ExampleComponent);
 
--- react-ts-dependency-injection-expeiment/src/react-injector/injectServices.tsx
+++ react-ts-dependency-injection-expeiment/src/react-injector/injectServices.tsx
@@ -9,24 +9,40 @@ interface ServiceCollection {
 export function injector<SC extends ServiceCollection>(services: SC) {
   const instances: any = {};
 
+  function getService(injectable: string): any {
+    if (typeof instances[injectable] === "undefined") {
+      const serviceCreator = services[injectable];
+      if (typeof serviceCreator !== "function") {
+        throw new Error(`the service '${injectable}' must be a function`);
+      }
+      instances[injectable] = serviceCreator();
+    }
+    return instances[injectable];
+  }
+
+  function useService<S extends Extract<keyof SC, string>, O extends ReturnType<SC[S]>>(injectable: S): O {
+    return getService(injectable);
+  }
+
+  function useServices<S extends Extract<keyof SC, string>, O extends ReturnType<SC[S]>>(...injectable: S[]): { [key in S]: O } {
+    const newProps: any = {};
+    for (let index = 0; index < injectable.length; index++) {
+      const key = injectable[index];
+      newProps[key] = getService(key);
+    }
+    return newProps;
+  }
+
   function withServices<S extends Extract<keyof SC, string>>(...injectable: S[]) {
 
     return function componentInjector<P extends { [key in S]: any }>(Component: ComponentType<P>): ComponentType<Omit<P, S>> {
 
       function ComponentWithServices(props: PropsWithChildren<Omit<P, S>>) {
-        const newProps: any = { ...props };
-        for (let index = 0; index < injectable.length; index++) {
-          const key = injectable[index];
-          if (typeof instances[key] === "undefined") {
-            const serviceCreator = services[key];
-            if (typeof serviceCreator !== "function") {
-              throw new Error(`the service '${key}' must be a function`);
-            }
-            instances[key] = serviceCreator();
-          }
-          newProps[key] = instances[key];
-        }
-        return <Component {...newProps as unknown as P} />;
+        const newProps: any = {
+          ...props,
+          ...useServices(...injectable)
+        };
+        return <Component {...newProps as P} />;
       }
 
       return ComponentWithServices;
@@ -34,6 +50,8 @@ export function injector<SC extends ServiceCollection>(services: SC) {
   }
 
   return {
-    withServices
+    withServices,
+    useServices,
+    useService
   }
 }
