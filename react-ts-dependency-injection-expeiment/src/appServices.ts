227feb543cca74fdb8008694f793b82e33fa1746
Viktor Faddi <faddiv@gmail.com>
2020-12-21 14:40:35 +0100

    Missing commit.

---
+++ react-ts-dependency-injection-expeiment/src/appServices.ts
@@ -0,0 +1,11 @@
+import { injector } from "./react-injector";
+
+export const appServiceCollection = {
+  service1: () => "Hello",
+  service2: () => 42,
+  service3: () => new String("from service"),
+  service4: () => "from service",
+  service5: () => { return { important: "Do remember" }; }
+};
+
+export const { withServices, useService, useServices, ServiceProvider } = injector(appServiceCollection);
