279837515d1389e846c6f82aec58950a6a5f4adf
faddiv <faddiv@gmail.com>
2022-01-09 20:54:58 +0100

    Generate directory entries.

--- ActiveDirectoryPlayground/ActiveDirectoryPlayground/Program.cs
+++ ActiveDirectoryPlayground/ActiveDirectoryPlayground/Program.cs
@@ -1,19 +1,11 @@
-﻿using System.DirectoryServices;
-using System.Net;
+using System.DirectoryServices;
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-var id = new System.DirectoryServices.Protocols.LdapDirectoryIdentifier("FADDIV", 50000);
+/*var id = new System.DirectoryServices.Protocols.LdapDirectoryIdentifier("FADDIV", 50000);
 using var c = new System.DirectoryServices.Protocols.LdapConnection(id);
 c.Bind();
 var response = (System.DirectoryServices.Protocols.SearchResponse)c.SendRequest(new System.DirectoryServices.Protocols.SearchRequest("CN=faddiv,DC=localtest,DC=me", "(displayName=Fad*)", System.DirectoryServices.Protocols.SearchScope.Subtree, new[] { "displayName", "employeeId" }));
 foreach (System.DirectoryServices.Protocols.SearchResultEntry item in response.Entries)
 {
     Console.WriteLine("{0} - {1}", item.Attributes["displayName"][0], item.Attributes["employeeId"][0]);
-}
-/*
-var r = new DirectoryEntry("LDAP://FADDIV:50000/CN=Users,CN=faddiv,DC=localtest,DC=me");
-var s = new DirectorySearcher(r, "(displayName=Fad*)", new[] { "displayName", "employeeId" }, SearchScope.Subtree);
-foreach (SearchResult item in s.FindAll())
-{
-    Console.WriteLine("{0} - {1}", item.Properties["displayName"][0], item.Properties["employeeId"][0]);
 }*/
