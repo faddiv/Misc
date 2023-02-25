dcdfaa9f59406d55b6b4244fbcab924de1e64378
Fadd Viktor <faddi.viktor@sonrisa.hu>
2023-02-25 13:14:53 +0100

    VS template continued.

--- ViteCommerce/ViteCommerce.UI/src/pages/counter/index.page.tsx
+++ ViteCommerce/ViteCommerce.UI/src/pages/counter/index.page.tsx
@@ -1,14 +1,19 @@
 import css from "./index.module.scss";
+import { useCallback, useState } from "react";
 
 export { Page }
 
 function Page() {
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = useCallback(() => {
+    setCounter(c => c + 1);
+  },[]);
   return (
-    <>
-      <h1>About</h1>
-      <p>
-        Example of using <code className={css.code}>vite-plugin-ssr</code>.
-      </p>
-    </>
-  )
+    <div>
+      <h1>Counter</h1>
+      <p>This is a simple example of a React component.</p>
+      <p aria-live="polite">Current count: <strong>{counter}</strong></p>
+      <button className="btn btn-primary" onClick={incrementCounter}>Increment</button>
+    </div>
+  );
 }
--- ViteCommerce/ViteCommerce.UI/src/pages/index/index.page.tsx
+++ ViteCommerce/ViteCommerce.UI/src/pages/index/index.page.tsx
@@ -4,35 +4,22 @@ import { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function Page({ data, error }: { data: any; error?: string }) {
-  const [count, setCount] = useState(0);
-  console.log("data", data);
   return (
-    <div className={css.app}>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className={css.card}>
-        <Button
-          variant="primary"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className={css.readTheDocs}>
-        Click on the Vite and React logos to learn more
-      </p>
-      {!!data && <p>Data: {JSON.stringify(data)}</p>}
-      {!!error && <p>Error: {error}</p>}
+    <div>
+      <h1>Hello, world!</h1>
+      <p>Welcome to your new single-page application, built with:</p>
+      <ul>
+        <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
+        <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
+        <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
+      </ul>
+      <p>To help you get started, we have also set up:</p>
+      <ul>
+        <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
+        <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
+        <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
+      </ul>
+      <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
     </div>
   );
 }
