6bd4aead93391dd9e80c80b8826412ed9f7eb862
faddiv <faddiv@gmail.com>
2024-04-28 20:00:23 +0200

    ParamsGenerator: Add parameter not collide test case.

--- ParamsSourceGenerator/SourceGeneratorTests/SourceGenerationTests.cs
+++ ParamsSourceGenerator/SourceGeneratorTests/SourceGenerationTests.cs
@@ -202,4 +202,12 @@ public class SourceGenerationTests
         await VerifyCS.VerifyGeneratorAsync(code,
             TestEnvironment.GetOuputs());
     }
+
+    [Fact]
+    public async Task Generate_WhenParametersDontCollide()
+    {
+        string code = TestEnvironment.GetValidSource();
+        await VerifyCS.VerifyGeneratorAsync(code,
+            TestEnvironment.GetOuputs());
+    }
 }
